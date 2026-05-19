1b55a9906a945fc73177a4e9c741d8e7cc1b7bf0
Nerea López <e.nerealr9@go.ugr.es>
2026-05-19 18:21:00 +0200

    mod1

--- database/migrations/create_lugar_table.php
+++ database/migrations/create_lugar_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('lugares', function(Blueprint $table){
             $table->id();
-            $table->string('lugar');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
